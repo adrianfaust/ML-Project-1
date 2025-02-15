62708fc879dff42ca2ad8f8027f77e404979be4c
adrianfaust <adrianbfaust@gmail.com>
2025-02-15 14:41:06 -0700

    Add perceptron_absorbed_bias implementation to main

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from matplotlib.colors import ListedColormap
 
 from src.adeline import adeline
 from src.perceptron import Perceptron
+from src.perceptron_absorbed_bias import PerceptronAbsorbedBias
 
 df = pd.read_csv(
     '../iris.data',
@@ -15,7 +16,8 @@ y = df.iloc[0:100, 4].values
 y = np.where(y == 'Iris-setosa', 0, 1)
 # extract sepal length and petal length
 
-X = df.iloc[0:100, [0, 2]].values
+X = df.iloc[0:100, [0, 1, 2,3]].values
+print(X)
 
 # plot data
 
@@ -36,6 +38,15 @@ ppn.fit(X, y)
 plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
 plt.xlabel('Epochs')
 plt.ylabel('Number of updates')
+plt.title('Perceptron')
+plt.show()
+
+ppn_absorbed = PerceptronAbsorbedBias()
+ppn.fit(X, y)
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of updates')
+plt.title('Perceptron Absorbed Bias')
 plt.show()
 
 ppa = adeline()
@@ -43,39 +54,63 @@ ppa.fit(X, y)
 plt.plot(range(1, len(ppa.losses_) + 1), ppa.losses_, marker='x')
 plt.xlabel('Epochs')
 plt.ylabel('Mean squared Error')
+plt.title('Adeline')
 plt.show()
 
+def plot_decision_regions_3d(X, y, classifier, resolution=0.02, show_after=False):
+    """Plots the decision regions for 3D data.
 
-def plot_decision_regions(X, y, classifier, resolution=0.02, show_after=False):
-    # setup marker generator and color map
-    markers = ('o', 's',
-               '^'
-               , 'v', '<')
+    Args:
+        X: Feature matrix of shape (n_samples, 3).
+        y: Target vector of shape (n_samples,).
+        classifier: Trained classifier object.
+        resolution: Resolution of the meshgrid.
+        show_after: Whether to immediately show the plot.
+    """
+
+    markers = ('o', 's', '^', 'v', '<')  # Add more if needed
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
-    # plot the decision surface
+
+    # Create meshgrid for 3D
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                           np.arange(x2_min, x2_max, resolution))
-    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    x3_min, x3_max = X[:, 2].min() - 1, X[:, 2].max() + 1  # Add z-axis limits
+
+    xx1, xx2, xx3 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                                 np.arange(x2_min, x2_max, resolution),
+                                 np.arange(x3_min, x3_max, resolution))
+
+    # Predict on the meshgrid
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel(), xx3.ravel()]).T)  # Include z
     lab = lab.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-    # plot class examples
+
+    # Create 3D plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plot the decision surface (using a scatter plot for 3D)
+    ax.scatter(xx1.ravel(), xx2.ravel(), xx3.ravel(), c=lab.ravel(), alpha=0.2, cmap=cmap) # Alpha for transparency
+
+    # Plot the data points
     for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0],
-                    y=X[y == cl, 1],
-                    alpha=0.8,
-                    c=colors[idx],
-                    marker=markers[idx],
-                    label=f'Class {cl}'
-                    ,
-                    edgecolor='black')
+        ax.scatter(X[y == cl, 0], X[y == cl, 1], X[y == cl, 2],
+                   alpha=0.8, c=colors[idx], marker=markers[idx], label=f'Class {cl}', edgecolor='black')
+
+    ax.set_xlabel('X1')
+    ax.set_ylabel('X2')
+    ax.set_zlabel('X3')  # Set z-axis label
+
+    ax.set_xlim(x1_min, x1_max)
+    ax.set_ylim(x2_min, x2_max)
+    ax.set_zlim(x3_min, x3_max)
+
+    ax.legend()
+
     if show_after:
         plt.show()
 
-plot_decision_regions(X, y, ppn)
-plot_decision_regions(X, y, ppa)
-plt.show()
+    return fig, ax
+
+# plot_decision_regions(X, y, ppn, show_after=True)
+# plot_decision_regions(X, y, ppa, show_after=True)
